8590eb54b8e30bb6c0afc704d2aee85ba8c564ff
KseniaEnot <58525970+KseniaEnot@users.noreply.github.com>
2025-03-08 21:06:37 +0300

    test swipe image
    add tem img, change yarn

--- Assets/Script/YarnController.cs
+++ Assets/Script/YarnController.cs
@@ -1,25 +1,37 @@
 using Yarn.Unity;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class YarnController : MonoBehaviour
 {
     [SerializeField]
     private DialogueRunner dialogueRunner;
+    private Image image;
 
-    /*private void Start()
+    public Sprite sprite1;
+    public Sprite sprite2;
+
+
+    private void Start()
     {
-        dialogueRunner.StartDialogue("TestDialogScript");
-    }*/
+        //dialogueRunner.StartDialogue("TestDialogScript");
+        image = gameObject.GetComponent<Image>();
+    }
 
     [YarnCommand("moveCircle")]
-    public IEnumerator MoveCircle(GameObject pos)
+    public void MoveCircle(int i)
     {
-        Vector3 startPos = transform.position;
-        for (int i = 0; i < 50; i++)
-        {
-            transform.position += Vector3.Lerp(startPos, pos.transform.position, 0.02f);
-            yield return new WaitForSeconds(0.1f);
-        }
+        Debug.Log("WORK!");
+        if (i == 1)
+            image.sprite = sprite2;
+        else
+            image.sprite = sprite1;
+        /*Vector3 startPos = transform.position;
+         for (int i = 0; i < 50; i++)
+         {
+             transform.position += Vector3.Lerp(startPos, pos.transform.position, 0.02f);
+             yield return new WaitForSeconds(0.1f);
+         }*/
     }
 }
